90cd820b735ec62e097ef36a5ce7c00469f56a33
mtahran <mtahran@akumoproject.com>
2024-02-11 23:13:57 +0000

    removed keys

--- PrimeX/providers.tf
+++ PrimeX/providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5UPRSOAWGUSS2JCH"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
